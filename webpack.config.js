3fc68b0b5a03c387a7964c3dfba1b371c1b103d8
markusmust <mustmark@tlu.ee>
2019-10-13 22:21:39 +0300

    esimene react

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,17 @@ module.exports = {
       }
     ])
   ],
+
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: 'babel-loader' 
+      }
+    ]
+  },
+
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
