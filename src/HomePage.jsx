1352861b6ab6993486b92a0c0c95b4851c194675
markusmust <mustmark@tlu.ee>
2019-10-14 00:59:52 +0300

    lesson 3 done

---
+++ src/HomePage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header.jsx";
+import ItemList from "./ItemList.jsx";
+import {phones, laptops} from "./mydatabase.js";
+
+class HomePage extends React.PureComponent{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			items: phones,
+		}
+	}
+
+	handleChange(event){
+		console.log(event.target.value);
+		switch (event.target.value) {
+			case "phones": {
+				this.setState({
+					items: phones,
+				});
+				break;
+			}
+			case "laptops": {
+				this.setState({
+					items: laptops,
+				});
+				break;
+			}
+		}
+		console.log("App state", this.state);
+	};
+
+	render(){
+		return (
+			<>
+				<Header/>
+				<select onChange={this.handleChange.bind(this)}>
+	    			<option value="phones">Phones</option>
+	    			<option value="laptops">Laptops</option>
+				</select>
+				<ItemList items={this.state.items} />
+			</>
+		)
+	}
+}
+
+export default HomePage;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HomePage from "./HomePage.jsx";
+import ItemPage from "./ItemPage.jsx";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const root = document.getElementById("app");
 
-console.log("root", root);
-
 ReactDOM.render(
-	<button>i am button, test</button>,
+	<BrowserRouter>
+		<Route path="/" exact component={HomePage} />
+		<Route path="/item" excat component={ItemPage} />
+	</BrowserRouter>,
 	root
 );
