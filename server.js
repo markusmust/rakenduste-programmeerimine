33dae08cd5522212bdc8c8d846c6fbc407123253
markusmust <mustmark@tlu.ee>
2019-10-14 04:03:12 +0300

    lab 4 yl 5 error 504

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const PORT = process.env.PORT || 3000;
 const DB = require("./server/database.js");
 
 // get all items
-app.get("/items", (req, res)=>{
+app.get("api/items", (req, res)=>{
 	res.json(DB.getItems())
 });
 
 //get items with id
-app.get("/items/:itemId", (req, res)=>{
+app.get("api/items/:itemId", (req, res)=>{
 	res.send(DB.getItem(req.params.itemId));
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-})
+});
 
 app.use(express.static('dist'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,9 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   }
 };
