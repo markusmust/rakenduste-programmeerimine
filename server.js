64d4b5a1c0fa86e962da3eecb1b2be086cf92216
markusmust <mustmark@tlu.ee>
2019-10-14 04:38:32 +0300

    lab 4 yl 5 done

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 3000;
 const DB = require("./server/database.js");
 
 // get all items
-app.get("api/items", (req, res)=>{
+app.get("/api/items", (req, res)=>{
 	res.json(DB.getItems())
 });
 
 //get items with id
-app.get("api/items/:itemId", (req, res)=>{
+app.get("/api/items/:itemId", (req, res)=>{
 	res.send(DB.getItem(req.params.itemId));
 });
 
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,59 +1,61 @@
 import React from "react";
 import Header from "./Header.jsx";
 import ItemList from "./ItemList.jsx";
-import {phones, laptops} from "./mydatabase.js";
+//import {phones, laptops} from "./mydatabase.js";
 
 class HomePage extends React.PureComponent{
 
 	constructor(props){
 		super(props);
 		this.state = {
-			items: phones,
+			items: [],
+			selectedCategory: "phones",
 		};
 	}
 
 	componentDidMount(){
-		fetch("http://localhost:9000/api/items")
+		this.fetchItems();
+	}
+
+fetchItems = () => {
+	fetch("/api/items")
 		.then(res =>{
 			console.log("res", res);
 			return res.json();
 		})
 		.then( items=> {
 			console.log("items", items);
+			this.setState({
+				items
+			});
 		})
 		.catch(err =>{
 			console.log("err", err);
 		});
+	};
+	handleDropdown(event){
+		console.log(event.target.value);
+		this.setState({
+			selectedCategory: event.target.value
+		});
+
+
 	}
 
-	handleChange(event){
-		console.log(event.target.value);
-		switch (event.target.value) {
-			case "phones": {
-				this.setState({
-					items: phones,
-				});
-				break;
-			}
-			case "laptops": {
-				this.setState({
-					items: laptops,
-				});
-				break;
-			}
-		}
-		console.log("App state", this.state);
+	getVisibleItems = () => {
+		return this.state.items.filter(item => item.category === this.state.selectedCategory);
 	}
 
 	render(){
+		console.log("this.state", this.state);
 		return (
 			<>
 				<Header/>
-				<select onChange={this.handleChange.bind(this)}>
+				<select onChange={this.handleDropdown.bind(this)}>
 					<option value="phones">Phones</option>
 					<option value="laptops">Laptops</option>
 				</select>
-				<ItemList items={this.state.items} />
+				<ItemList items={this.getVisibleItems()} />
 			</>
 		);
 	}
