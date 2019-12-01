77d7c743d9ba8d6691913efa508227750932ce83
markusmust <mustmark@tlu.ee>
2019-12-01 23:45:10 +0200

    lesson 5 mongodb done

--- server.js
+++ server.js
@@ -2,48 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
-const DB = require("./server/database.js");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRouter = require("./server/user.js");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-e1ug4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-var kittySchema = new mongoose.Schema({
-  name: String
-});
-
-var Kitten = mongoose.model("Kitten", kittySchema);
-
-var kitten1 = new Kitten ({
-  name: "red cat"
-});
 
 
+app.use(userRouter);
 
-mongoose.connect(DB_URL)
-  .then(() =>{
-    console.log("Database access success!");
-    kitten1.save( err =>{
-      if(err){
-        console.error("we had an error");
-      }else{
-        console.log("Success save!");
-      }
-    });
-  })
-  .catch( err =>{
-    console.error("error happened", err);
-  });
-
-// get all items
-app.get("/api/items", (req, res)=>{
-	res.json(DB.getItems());
-});
-
-//get items with id
-app.get("/api/items/:itemId", (req, res)=>{
-	res.send(DB.getItem(req.params.itemId));
-});
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
@@ -55,8 +23,20 @@ app.get("/items/*", (req, res) => {
 
 app.use(express.static("dist"));
 
-// Heroku needs process.env.PORT
-app.listen(PORT, () => {
-  console.log("Server started", PORT);
-  console.log(`http://localhost:${PORT}`);
-});
+function listen(){
+  // Heroku needs process.env.PORT
+  app.listen(PORT, () => {
+    console.log("Server started", PORT);
+    console.log(`http://localhost:${PORT}`);
+  });
+}
+
+
+mongoose.connect(DB_URL)
+  .then(() =>{
+    console.log("Database access success!");
+    listen();
+  })
+  .catch( err =>{
+    console.error("error happened", err);
+  });
---
+++ server/user.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const router = express.Router();
+const DB = require("./database.js");
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+    imgSrc: { type: String, required: true},
+    title: { type: String, required: true},
+    price: { type: Number, required: true},
+    category: { type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+router.post("/api/items", (req, res) =>{
+    const props = {
+        imgSrc: "google.com",
+        title: "phone red",
+        price: 200,
+        category: "phones",
+    };
+    const item1 = new Item(props);
+    item1.save( err =>{
+        if(err){
+            console.log("Error:",err);
+            res.send(500);
+            return;
+        }
+        console.log("Success Create!");
+        res.send(201);
+    });
+});
+
+// return an item
+router.get("/api/items/::itemId", (req,res)=>{
+    Item.findById(req.params.itemId, function (err, item){
+        if(err){
+            console.log("Error", err);
+            res.status(500).send(err);
+            return;
+        }
+        res.send(item);
+    });
+});
+
+// returns all items
+router.get("/api/items", (req, res)=>{
+    Item.find({}, function(err, items) {
+        if(err){
+            console.log("Error", err);
+            res.status(500).send(err);
+            return;
+        }
+        res.send(items);
+    });
+});
+
+module.exports = router;
+
+/* 
+// get all items
+router.get("/api/items", (req, res)=>{
+	res.json(DB.getItems());
+});
+
+//get items with id
+router.get("/api/items/:itemId", (req, res)=>{
+	res.send(DB.getItem(req.params.itemId));
+}); */
