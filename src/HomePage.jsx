d9bed8f667a54190c3251f10b0cea68164a4a83b
markusmust <mustmarkus0@gmail.com>
2019-12-01 21:28:51 +0200

    task 5 done

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,114 +6,128 @@ import SortDropdown from "./SortDropdown.jsx";
 import PropTypes from "prop-types";
 import "./homepage.css";
 
-
 class HomePage extends React.PureComponent{
 
-	constructor(props){
-		super(props);
-		this.state = {
-			sortDirection:-1,
-			items: [],
-			allCategories: ["phones", "laptops"],
-			selectedCategories: ["phones"],
-		};
-	}
+    constructor(props){
+        super(props);
+        this.state = {
+            sortDirection: -1,
+            items: [],
+            allCategories: ["phones", "laptops"],
+            selectedCategories: ["phones"],
+        };
+    }
 
-	componentDidMount(){
-		this.fetchItems();
-	}
+    componentDidMount(){
+        this.fetchItems();
+    }
 
-fetchItems = () => {
-	fetch("/api/items")
-		.then(res =>{
-			console.log("res", res);
-			return res.json();
-		})
-		.then( items=> {
-			console.log("items", items);
-			this.setState({
-				items
-			});
-		})
-		.catch(err =>{
-			console.log("err", err);
-		});
-	};
+    fetchItems = () => {
+        fetch("/api/items")
+        .then(res =>{
+            console.log("res", res);
+            return res.json();
+        })
+        .then(items => {
+            console.log("items", items);
+            this.setState({
+                items
+            });
+        })
+        .catch(err => {
+            console.log("err", err);
+        });
+    }
 
-	handleSortDropdown = (event) => {
-	this.setState({
-		sortDirection: parseInt(event.target.value),
-	});
-};
+    handleSortDropdown = (event) => {
+        this.setState({
+            sortDirection: parseInt(event.target.value),
+        });
+    }
 
-	handleDropdown = (event) => {
-		if(this.isSelected(event.target.name)){
-			const clone = this.state.selectedCategories.slice();
-			const index = this.state.selectedCategories.indexOf(event.target.name);
-			clone.splice(index, 1);
-			this.setState({
-				selectedCategories: clone
-			});
-		}
-		else{
-			this.setState({
-				selectedCategories: this.state.selectedCategories.concat([event.target.name])
-			});
-		}
-	}
+    handleDropdown = (event) => {
+        // console.log(event.target.value, event.target.name);
+        if(this.isSelected(event.target.name)){
+            const clone = this.state.selectedCategories.slice();
+            // console.log("targetname:", event.target.name);
+            const index = this.state.selectedCategories.indexOf(event.target.name);
+            clone.splice(index, 1);
+            this.setState({
+                selectedCategories: clone
+            });
+        }
+        else{
+            this.setState({
+                selectedCategories: this.state.selectedCategories.concat([event.target.name])
+            });
+        }
+    }
 
-	getVisibleItems = () => {
-		return this.state.items.filter(item => this.isSelected(item.category));
-	}
+    getVisibleItems = () => {
+        return this.state.items
+        .filter(item => this.isSelected(item.category))
+        .sort((a, b) => {
+            switch (this.state.sortDirection) {
+                case -1: return b.price -a.price;
+                case 1: return a.price -b.price;
+            }
+        });
+    }
 
-  isSelected = (name) => this.state.selectedCategories.indexOf(name) >= 0;
+    isSelected = (name) => this.state.selectedCategories.indexOf(name) >= 0;
 
-	render(){
-		console.log("this.state", this.state);
-		return (
-			<>
-				<Header/>
-				<ItemFilters 
-					allCategories={this.state.allCategories}
-					handleDropdown={this.handleDropdown}
-					isSelected={this.isSelected}
-				/>
-				<div className={"items-settings"}>
-				<SortDropdown 
-						direction={this.state.sortDirection}
-						onChange={this.handSortDropdown}
-					/>
-				</div>	
-				<ItemList items={this.getVisibleItems()} />
-			</>
-		);
-	}
+    render(){
+        const items = this.getVisibleItems();
+        return (
+            <>
+            <div className="body-wrapper">
+                <Header/>
+                <div className="filters-wrapper">
+                    <ItemFilters
+                    allCategories={this.state.allCategories}
+                    handleDropdown={this.handleDropdown}
+                    isSelected={this.isSelected}
+                    />
+                </div>
+                <div className="items-header-wrapper">
+                    <div>
+                        Found: {items.length} {" "}
+                        {this.state.selectedCategories.join(", ")}
+                    </div>
+                    <SortDropdown
+                    direction={this.state.sortDirection}
+                    onChange={this.handleSortDropdown}/>
+                </div>
+                <ItemList items={items}/>
+            </div>
+            </>
+        );
+    }
 }
 
 const ItemFilters = ({allCategories, handleDropdown, isSelected}) => {
-	return (
-	<div className={"itemFilters-wrapper"}>
-		{
-			allCategories.map( categoryName => {
-				return (
-					<Checkbox
-						key={categoryName}
-						name={categoryName}
-						onChange={handleDropdown}
-						checked={isSelected(categoryName)}
-					/>  
-				);
-			})
-		}
-	</div>
-	);
+    return (
+        <>
+        {
+            allCategories.map(categoryName => {
+                return (
+                    <Checkbox
+                    key={categoryName}
+                    name={categoryName}
+                    onChange={handleDropdown}
+                    checked={isSelected(categoryName)}
+                    />
+                );
+            })
+        }
+        </>
+    );
 };
 
 ItemFilters.propTypes = {
-	allCategories: PropTypes.array.isRequired,
-	handleDropdown: PropTypes.func.isRequired,
-	isSelected: PropTypes.func.isRequired,
+    allCategories: PropTypes.array.isRequired,
+    handleDropdown: PropTypes.func.isRequired,
+    isSelected: PropTypes.func.isRequired,
 };
-
 
 export default HomePage;
--- src/SortDropdown.jsx
+++ src/SortDropdown.jsx
@@ -1,20 +1,19 @@
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import "./dropdown.css";
 
-
-const SortDropdown = ({direction,onChange}) => (
-	<div>
-		<select onChange={onChange} value={direction}>
-			<option value={-1}>Price high to low</option>
-			<option value={1}>Price low to high</option>
-		</select>	
-	</div>	
-
+const SortDropdown = ({direction, onChange}) => (
+<span className="custom-dropdown big">
+    <select value={direction} onChange={onChange}>  
+        <option value={1}>Price: Low to high</option>
+        <option value={-1}>Price: High to low</option>
+    </select>
+</span>
 );
 
 SortDropdown.propTypes = {
-	direction: PropTypes.oneOf([1, -1]).isRequired,
-	onChange: PropTypes.func.isRequired,
+    direction: PropTypes.oneOf([1, -1]).isRequired,
+    onChange: PropTypes.func.isRequired,
 };
 
 export default SortDropdown;
